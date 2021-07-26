76c994827956ab6cb63d398b5098a1a3e9a0fe90
ecinauce <nicololynsuecuacion@gmail.com>
2021-07-26 15:09:23 +0800

    [DEBUG] Fixed views js.

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 // vue.config.js file to be place in the root of your repository
 
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/'
-  //   : '/',
-  mode: 'development'
+  publicPath: '/ecinauce.github.io/'
 }
